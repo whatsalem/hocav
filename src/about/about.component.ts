c6b6f7bcd583531361248a5e566e0a16f263249f
whatsalem <zolekwhatsalem@gmail.com>
2018-04-04 20:55:38 +0700

    sddsdsds

--- src/about/about.component.ts
+++ src/about/about.component.ts
@@ -20,14 +20,18 @@ email:any;
   {
   this.http.get('https://bansachonline.herokuapp.com/getlistuser').subscribe(data => {
       this.results = data;
-      console.log(data);
-      console.clear();
+    });
+  }
+  delete(id)
+  {
+  this.http.delete('https://bansachonline.herokuapp.com/user/'+id).subscribe(data => {
+      this.results = data;
+      this.load();
     });
   }
   onSubmit(form: NgForm){
     this.http.post("https://bansachonline.herokuapp.com/user",{user_name_add: this.name,user_email_add: this.email})
     .subscribe(data => {
-      console.log(data);
       this.load();
     });
   }
